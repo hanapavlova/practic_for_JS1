9fe2cc5773bf21ab2568d72ae49f65a6030821df
Hana Pavlova <hanapavlova7@gmail.com>
2024-03-22 14:28:23 +0100

    prvni task

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,36 @@
 console.log("task 1");
+//Vytvořte proměnnou counterValue, která bude ukládat aktuální hodnotu čítače a inicializuje ji na 0.
+//K tlačítkům přidejte posluchače kliknutí, uvnitř kterých zvyšujete nebo snižujete hodnotu čítače.
+//Aktualizujte rozhraní novou hodnotou proměnné counterValue.
+
+//aktualizace tlačítka decrement
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+let clickCountDecrement = -2;
+
+const decrement = () => {
+buttonDecrement.textContent = clickCountDecrement
+clickCountDecrement--
+}
+buttonDecrement.addEventListener('click', decrement);
+
+
+//aktualize tlačítka increment
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+let clickCountIncrement = 2;
+
+const increment = () => {
+    buttonIncrement.textContent = clickCountIncrement
+    clickCountIncrement++
+    }
+buttonIncrement.addEventListener('click', increment);
+
+
+//aktualizace čítače
+
+const counter = document.getElementById('value')
+
+const counterValue = () => {
+   counter.textContent = (clickCountDecrement + clickCountIncrement)
+}
+
+document.addEventListener("click", counterValue)
